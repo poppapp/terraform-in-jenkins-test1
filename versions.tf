0846f9d22178ca5f34014a3d35fe6d62bbdf587b
poppapp <46346886+poppapp@users.noreply.github.com>
2022-06-24 17:53:49 +0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 # Terraform Block
 terraform {
-  required_version = "~> 1.1"
+  required_version = "1.1"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.12"
+      version = "4.12"
     }
   } 
 }  
