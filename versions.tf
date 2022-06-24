742982765b6b7617b0be7e453189bb8c95354fc2
KPN <popwings01@gmail.com>
2022-06-24 18:21:03 +0700

    mm

--- versions.tf
+++ versions.tf
@@ -1,15 +1,4 @@
 # Terraform Block
 terraform {
   required_version = "1.1"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.12"
-    }
-  } 
 }  
-
-# Provider Block
-provider "aws" {
-  region  = var.aws_region
-}
